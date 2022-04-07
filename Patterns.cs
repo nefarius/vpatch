c5baab7ccff46e1ea9c5f0af65a84f240a9d3f5f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-04-07 22:17:23 +0200

    Clean-up

--- Patterns.cs
+++ Patterns.cs
@@ -6,30 +6,30 @@ namespace vpatch
     {
         public static KeyValuePair<string, string> AssemblyVersion
             =>
-                new KeyValuePair<string, string>(@"^ *\[assembly[\: ]*AssemblyVersion.*\]",
+                new(@"^ *\[assembly[\: ]*AssemblyVersion.*\]",
                     "[assembly: AssemblyVersion(\"{0}\")]");
 
         public static KeyValuePair<string, string> AssemblyFileVersion =>
-            new KeyValuePair<string, string>(@"^ *\[assembly[\: ]*AssemblyFileVersion.*\]",
+            new(@"^ *\[assembly[\: ]*AssemblyFileVersion.*\]",
                 "[assembly: AssemblyFileVersion(\"{0}\")]");
 
         public static KeyValuePair<string, string> ResourceFileVersion =>
-            new KeyValuePair<string, string>(@"FILEVERSION[ \\t]*\d*,\d*,\d*,\d*", "FILEVERSION {0}");
+            new(@"FILEVERSION[ \\t]*\d*,\d*,\d*,\d*", "FILEVERSION {0}");
 
         public static KeyValuePair<string, string> ResourceProductVersion =>
-            new KeyValuePair<string, string>(@"PRODUCTVERSION[ \\t]*\d*,\d*,\d*,\d*", "PRODUCTVERSION {0}");
+            new(@"PRODUCTVERSION[ \\t]*\d*,\d*,\d*,\d*", "PRODUCTVERSION {0}");
 
         public static KeyValuePair<string, string> ResourceStringFileVersion
-            => new KeyValuePair<string, string>("^[ \\t]*VALUE[ \\t]*\"FileVersion\"[ \\t]*,[ \\t]*\"\\d*\\.\\d*\\.\\d*\\.\\d*\"",
+            => new("^[ \\t]*VALUE[ \\t]*\"FileVersion\"[ \\t]*,[ \\t]*\"\\d*\\.\\d*\\.\\d*\\.\\d*\"",
                 "VALUE \"FileVersion\", \"{0}\"");
 
         public static KeyValuePair<string, string> ResourceStringProductVersion
-            => new KeyValuePair<string, string>("^[ \\t]*VALUE[ \\t]*\"ProductVersion\"[ \\t]*,[ \\t]*\"\\d*\\.\\d*\\.\\d*\\.\\d*\"",
+            => new("^[ \\t]*VALUE[ \\t]*\"ProductVersion\"[ \\t]*,[ \\t]*\"\\d*\\.\\d*\\.\\d*\\.\\d*\"",
                 "VALUE \"ProductVersion\", \"{0}\"");
 
         public static KeyValuePair<string, string> VcxprojInfTimeStamp
             =>
-                new KeyValuePair<string, string>(
+                new(
                     "<Inf>(\\n|\\r|\\r\\n) *<TimeStamp>\\d*\\.\\d*\\.\\d*\\.\\d*<\\/TimeStamp>(\\n|\\r|\\r\\n) *<\\/Inf>",
                     "<Inf>\r\n      <TimeStamp>{0}</TimeStamp>\r\n    </Inf>");
     }
