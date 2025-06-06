4559a4ae2784478d20c9ad8877dfcb72d94b4c63
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-06-06 19:11:15 +0200

    Added exit code propagation on error

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 using CommandLine;
@@ -10,79 +11,93 @@ namespace vpatch;
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 internal class Program
 {
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
-        MSBuildLocator.RegisterDefaults();
+        try
+        {
+            MSBuildLocator.RegisterDefaults();
 
-        Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(o =>
-            {
-                o.UseVersionFromFile();
-
-                #region .NET Framework specific
-                
-                if (o.OverwriteAssemblyVersion)
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
                 {
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.AssemblyVersion.Key,
-                        string.Format(Patterns.AssemblyVersion.Value, o.Version));
-                }
+                    o.UseVersionFromFile();
 
-                if (o.OverwriteAssemblyFileVersion)
-                {
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.AssemblyFileVersion.Key,
-                        string.Format(Patterns.AssemblyFileVersion.Value, o.Version));
-                }
+                    #region .NET Framework specific
 
-                #endregion
+                    if (o.OverwriteAssemblyVersion)
+                    {
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.AssemblyVersion.Key,
+                            string.Format(Patterns.AssemblyVersion.Value, o.Version));
+                    }
 
-                #region C++/Driver project specific
+                    if (o.OverwriteAssemblyFileVersion)
+                    {
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.AssemblyFileVersion.Key,
+                            string.Format(Patterns.AssemblyFileVersion.Value, o.Version));
+                    }
 
-                string versionString = string.IsNullOrEmpty(o.VersionString) ? o.Version.ToString() : o.VersionString;
-                
-                if (o.OverwriteResourceFileVersion)
-                {
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceFileVersion.Key,
-                        string.Format(Patterns.ResourceFileVersion.Value, o.Version.ToString().Replace('.', ',')),
-                        Encoding.Default);
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceStringFileVersion.Key,
-                        string.Format(Patterns.ResourceStringFileVersion.Value, versionString), Encoding.Default);
-                }
+                    #endregion
 
-                if (o.OverwriteResourceProductVersion)
-                {
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceProductVersion.Key,
-                        string.Format(Patterns.ResourceProductVersion.Value,
-                            o.Version.ToString().Replace('.', ',')),
-                        Encoding.Default);
-                    TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceStringProductVersion.Key,
-                        string.Format(Patterns.ResourceStringProductVersion.Value, versionString), Encoding.Default);
-                }
+                    #region C++/Driver project specific
 
-                if (o.OverwriteVcxprojInfTimeStamp)
-                {
-                    TextFileHelper.PatchInfFile(o.TargetFile, o.Version);
-                }
-                
-                #endregion
+                    string versionString =
+                        string.IsNullOrEmpty(o.VersionString) ? o.Version.ToString() : o.VersionString;
 
-                #region .NET Core specific
+                    if (o.OverwriteResourceFileVersion)
+                    {
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceFileVersion.Key,
+                            string.Format(Patterns.ResourceFileVersion.Value, o.Version.ToString().Replace('.', ',')),
+                            Encoding.Default);
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceStringFileVersion.Key,
+                            string.Format(Patterns.ResourceStringFileVersion.Value, versionString), Encoding.Default);
+                    }
 
-                if (o.OverwriteNetCoreAssemblyVersion)
-                {
-                    TextFileHelper.PatchProjectFile(o.TargetFile, "AssemblyVersion", o.Version.ToString());
-                }
+                    if (o.OverwriteResourceProductVersion)
+                    {
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceProductVersion.Key,
+                            string.Format(Patterns.ResourceProductVersion.Value,
+                                o.Version.ToString().Replace('.', ',')),
+                            Encoding.Default);
+                        TextFileHelper.RegexReplace(o.TargetFile, Patterns.ResourceStringProductVersion.Key,
+                            string.Format(Patterns.ResourceStringProductVersion.Value, versionString),
+                            Encoding.Default);
+                    }
 
-                if (o.OverwriteNetCoreFileVersion)
-                {
-                    TextFileHelper.PatchProjectFile(o.TargetFile, "FileVersion", o.Version.ToString());
-                }
+                    if (o.OverwriteVcxprojInfTimeStamp)
+                    {
+                        TextFileHelper.PatchInfFile(o.TargetFile, o.Version);
+                    }
 
-                if (o.OverwriteNetCoreVersion)
-                {
-                    TextFileHelper.PatchProjectFile(o.TargetFile, "Version", o.Version.ToString());
-                }
+                    #endregion
 
-                #endregion
-            });
+                    #region .NET Core specific
+
+                    if (o.OverwriteNetCoreAssemblyVersion)
+                    {
+                        TextFileHelper.PatchProjectFile(o.TargetFile, "AssemblyVersion", o.Version.ToString());
+                    }
+
+                    if (o.OverwriteNetCoreFileVersion)
+                    {
+                        TextFileHelper.PatchProjectFile(o.TargetFile, "FileVersion", o.Version.ToString());
+                    }
+
+                    if (o.OverwriteNetCoreVersion)
+                    {
+                        TextFileHelper.PatchProjectFile(o.TargetFile, "Version", o.Version.ToString());
+                    }
+
+                    #endregion
+                });
+
+            Environment.ExitCode = 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Environment.ExitCode = 1;
+        }
+
+        return Environment.ExitCode;
     }
 }
